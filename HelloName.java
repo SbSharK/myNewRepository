b319eb09dda6fc3bf45968fe7deb95df03fe43f6
Chetan Shinde <schetan97@yahoo.com>
2021-08-17 20:31:40 +0530

    Add files via upload

---
+++ HelloName.java
@@ -0,0 +1,10 @@
+public class HelloName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, My name is Chetan Shinde.I am from Pune"); //CHANGES
+		System.out.println("I work in Apisero");
+		System.out.println("I am currently attending Greenfield training")
+	}
+
+}
